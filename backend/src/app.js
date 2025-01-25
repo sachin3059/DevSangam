c051efc02a66230aa02d72df7f56a9d5f90e7654
sachin3059 <sachin54077kumar@gmail.com>
2025-01-26 00:08:42 +0530

    done with user api

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,6 +19,57 @@ app.post('/signup', async (req, res) => {
 });
 
 
+app.get("/user", async(req, res) => {
+    const userEmail = req.body.email;
+    try {
+       const user = await User.find({email: userEmail}); 
+       if(user.length === 0){
+        res.status(404).send("user not found");
+       }
+       else{
+        res.send(user);
+       }
+    } catch (error) {
+        res.status(400).send('Error to get user' +  error.message);
+    }
+})
+
+app.get("/feed", async(req, res) => {
+    try {
+        const data = await User.find({});
+        res.send(data);   
+    } catch (error) {
+        res.status(400).send('Error feed the user', + error.message);
+    }
+})
+
+app.delete('/user', async(req, res) => {
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        // or 
+        // const user = await User.findByIdAndDelete({_id: userId});
+        res.send('user deleted successfully');
+    } catch (error) {
+        res.status(400).send('error while delete user' + error.message);
+    }
+})
+
+
+app.patch("/user", async(req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    //console.log(data);
+    try {
+        const user = await User.findByIdAndUpdate({_id: userId}, data, {returnDocument: "before"}); 
+        console.log(user); // here older version of user is printed
+        res.send('updated user firstName');
+    } catch (error) {
+        res.status(400).send("error while updating user" + error.message);
+    }
+})
+
+
 connectDB().then( () => {
     console.log("Database connected successfully!");
     app.listen(3000, () => {
---
+++ guide.md
@@ -0,0 +1,5 @@
+
+
+- JS object vs json (difference)
+- Add the express.json to your app
+- Make your signup api
