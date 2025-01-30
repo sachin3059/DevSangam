28a71cc665b174ecb7f57967f963221be3cbef3d
sachin3059 <sachin54077kumar@gmail.com>
2025-01-30 11:24:18 +0530

    rectify some error

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants';
+import { useDispatch, useSelector } from 'react-redux'
+import { addFeed } from '../utils/feedSlice';
+import UserCard from './userCard';
 
 const Feed = () => {
-  return (
+  const feed = useSelector( (store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if(feed) return ;
+    try {
+      const res = await axios.get( BASE_URL + "/feed", {withCredentials: true});
+      dispatch(addFeed(res.data));
+      //console.log(res?.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  useEffect( () => {
+    getFeed();
+  }, []);
+
+
+  return feed && (
     <div>
-      <h1>Feed</h1>
+      <UserCard user={feed?.data[0]} />
     </div>
   )
 }
